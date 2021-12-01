337016746586e76a332527155ba17f2a4506b782
Sandra Duque <sduque@ideasoft.uy>
2021-12-01 10:46:23 -0300

    Ajustes por temas agregacion

--- pentaho/pdi/bin/APBLoad.bat
+++ pentaho/pdi/bin/APBLoad.bat
@@ -1,7 +1,7 @@
 c:
 cd C:\Develop\pentaho\data-integration
 
-Kitchen.bat  /logfile:C:\projects\ideasoft\data-tools-main\pentaho\pdi\log\apb.log /file:C:\projects\ideasoft\data-tools-main\pentaho\pdi\APB-Load.kjb
+Kitchen.bat  /logfile:C:\projects\ideasoft\data-tools\pentaho\pdi\log\apb.log /file:C:\projects\ideasoft\data-tools\pentaho\pdi\APB-Load.kjb
 
-cd \projects\ideasoft\data-tools-main\pentaho\pdi\bin
+cd \projects\ideasoft\data-tools\pentaho\pdi\bin
 
